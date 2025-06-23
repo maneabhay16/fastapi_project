193b39890fd9f68ff20cdeb53f01028ccea30acd
abhaymaneprepstudy <abhay.mane@prep.study>
2025-06-23 16:26:13 +0530

    added env and containarised celery and rabitmq

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-# check for postgress and setup pariksha and prepstudy so that can plugin 
-# add testing locust
-#write test cases 
+
+env_mode = os.getenv("ENV", "local")
+
+load_dotenv(dotenv_path=f".env.{env_mode}")
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
--- app/tasks/celery_worker.py
+++ app/tasks/celery_worker.py
@@ -1,14 +1,12 @@
 from celery import Celery
+import os
 
-from celery import Celery
+broker_url = os.getenv("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//")
 
-celery_app = Celery(
-    "worker",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
-)
+celery_app = Celery("worker", broker=broker_url, backend=broker_url)
 
-
+celery_app.conf.task_acks_late = True           
+celery_app.conf.worker_prefetch_multiplier = 1 
 celery_app.conf.task_routes = {
     "app.tasks.email.send_welcome_email": {"queue": "emails"},
 }
