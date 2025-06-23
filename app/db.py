93603fc193749cb22f291f0e2c3a5396ada9f99b
abhaymaneprepstudy <abhay.mane@prep.study>
2025-06-23 09:36:35 +0530

    added docker changes

--- app/db.py
+++ app/db.py
@@ -1,6 +1,8 @@
+import os
 from sqlmodel import SQLModel, create_engine
 from app.core.config import DATABASE_URL
 
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
